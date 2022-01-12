6195ccefb3e92d13d6d4b038ccbdcc356259edae
Rheaazarraga <rheaazarraga@gmail.com>
2022-01-12 22:00:14 +0000

    Fix sytnax error on line 7 - @article needed to be an instance variable because the comment is tied to a single (instance) article

--- app/controllers/comments_controller.rb
+++ app/controllers/comments_controller.rb
@@ -4,7 +4,7 @@ class CommentsController < ApplicationController
   def create
     @article = Article.find(params[:article_id])
   # each request for a comment has to keep track of the article to which the comment is attached - uses the find method of the Article model to get the article in question.
-    @comment = article.comments.create(comment_params)
+    @comment = @article.comments.create(comment_params)
   # creates and saves the comment, and automatically links the comment so that it belongs to that particular article
     redirect_to article_path(@article)
   # once the comment is created, the user is sent back to the original article with the above path
