768c2e81f1b0dbf7be0d9ae930a24c5f3cefc255
Rheaazarraga <rheaazarraga@gmail.com>
2022-01-12 21:06:09 +0000

    Add notes for reference

--- db/migrate/20220112205709_create_comments.rb
+++ db/migrate/20220112205709_create_comments.rb
@@ -4,7 +4,7 @@ class CreateComments < ActiveRecord::Migration
       t.string :commenter
       t.text :body
       t.references :article, index: true, foreign_key: true
-
+# t.references creates an integer column called article_id, and index for it, and a FK constraint that points to the articles table
       t.timestamps null: false
     end
   end
