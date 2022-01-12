e903edeeb53ba1f8af983428d3b60f39cac007c3
Rheaazarraga <rheaazarraga@gmail.com>
2022-01-12 04:02:19 +0000

    Generate assets files, helper files and migration files

--- app/controllers/articles_controller.rb
+++ app/controllers/articles_controller.rb
@@ -1,2 +1,10 @@
+# A controller is a class that is defined to inherit from ApplicationController. Methods defined in this class will become the actions for this controller. These actions will perform CRUD operations on the articles within our system
+
 class ArticlesController < ApplicationController
+  def new
+  end
+
+  def create
+    render plain: params[:article].inspect
+  end
 end
