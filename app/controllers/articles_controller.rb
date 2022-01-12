b1ca662681764a45385e4c946e1eb6401599caf2
Rheaazarraga <rheaazarraga@gmail.com>
2022-01-12 05:16:50 +0000

    Add edit action

--- app/controllers/articles_controller.rb
+++ app/controllers/articles_controller.rb
@@ -1,18 +1,31 @@
 # A controller is a class that is defined to inherit from ApplicationController. Methods defined in this class will become the actions for this controller. These actions will perform CRUD operations on the articles within our system
 
 class ArticlesController < ApplicationController
+  def index
+    @articles = Article.all
+  end
+
   def show
     @article = Article.find(params[:id])
   end
 
   def new
+    @article = Article.new
+  end
+
+  def edit
+    @article = Article.find(params[:id])
   end
 
   def create
     @article = Article.new(article_params)
 
-    @article.save
-    redirect_to @article
+    if @article.save
+      redirect_to @article
+    else
+      render 'new'
+    end
+  # use render instead of redirect_to when save returns false. The render method is used so that the @article object is passed back to the new template when it is rendered - rendering is done within the same request as the form submission, whereas the redirect_to will tell the browser to issue another request
   end
 
   private
