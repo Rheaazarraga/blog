5ef4d0314879033e09481b8a530d7a6be2364fc0
Rheaazarraga <rheaazarraga@gmail.com>
2022-01-12 22:25:07 +0000

    Add dependent option to handle successful deletion of all comments associated with a deleted article

--- app/models/article.rb
+++ app/models/article.rb
@@ -1,5 +1,7 @@
 class Article < ActiveRecord::Base
-  has_many :comments
+  has_many :comments, dependent :destroy
   validates :title, presence: true,
                     length: { minimum: 5 }
 end
+
+# when deleting an article, its associated comments will also need to be deleted otherwise they would clutter the db - use the dependent option of association
